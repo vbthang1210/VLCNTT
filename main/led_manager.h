1dfd6811fab42f2d70a61e2b14a684f41fd1d07c
Théng <thangtccy@gmail.com>
2026-08-18 19:14:07 +0700

    Remove duplicated LED pin definition

--- main/led_manager.h
+++ main/led_manager.h
@@ -1,8 +1,6 @@
 #ifndef LED_MANAGER_H
 #define LED_MANAGER_H
 
-#define LED_PIN 2
-
 #include <Arduino.h>
 
 class LedManager {
@@ -10,12 +8,9 @@ public:
     explicit LedManager(uint8_t pin);
 
     void begin();
-
     void turnOn();
     void turnOff();
-
     void setState(bool on);
-
     bool isOn() const;
 
 private:
